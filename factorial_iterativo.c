b52a6ffe5fa584dc0cd7e3b35c57ccf5f58a12ac
LauraVargas03 <lauravaleriavargasgomez@gmail.com>
2025-03-07 00:14:01 -0500

    Update factorial_iterativo.c

--- factorial_iterativo.c
+++ factorial_iterativo.c
@@ -1,20 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
-unsigned long long factorial_iterativo(int n, int *memoria_usada) {
+unsigned long long factorial_iterativo(int n) {
     unsigned long long resultado = 1;
-
     for (int i = 1; i <= n; i++) {
-       
-        unsigned long long *memoria = (unsigned long long *)malloc(sizeof(unsigned long long));
-        (*memoria_usada)++;  
-        
         resultado *= i;
-
-        free(memoria); 
     }
-
     return resultado;
 }
 
@@ -28,18 +19,15 @@ int main() {
         return 1;
     }
 
-    int memoria_usada = 0;
     clock_t start, end;
-
-    start = clock();
-    unsigned long long resultado = factorial_iterativo(num, &memoria_usada);
-    end = clock();
+    start = clock();  // Iniciar medición de tiempo
+    unsigned long long resultado = factorial_iterativo(num);
+    end = clock();  // Finalizar medición de tiempo
 
     double tiempo_ejecucion = ((double)(end - start)) / CLOCKS_PER_SEC;
-
+    
     printf("Factorial iterativo de %d es: %llu\n", num, resultado);
     printf("Tiempo de ejecución: %f segundos\n", tiempo_ejecucion);
-    printf("Memoria usada (aprox. iteraciones): %d bytes\n", memoria_usada * (int)sizeof(unsigned long long));
 
     return 0;
 }
